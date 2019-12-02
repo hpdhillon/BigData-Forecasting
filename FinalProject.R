f9655a0ba7c71764cc5e7b531cea07b24d4aa537
hpdhillon <57342519+hpdhillon@users.noreply.github.com>
2019-12-02 15:06:11 -0500

    FinalProject
    My code for the final project in Forecasting & Big Data, along with other forecasting attempts.

--- FinalProject.R
+++ FinalProject.R
@@ -1,8 +1,10 @@
 data = read.csv("Oil.csv")
+#import necessary libraries
 library(forecast)
 library(urca)
 library(vars)
 library(Metrics)
+#set up univariate analysis
 WTI = data$WTI
 plot(WTI)
 hist(WTI, breaks=20, freq=FALSE, col="blue")
@@ -13,30 +15,37 @@ seasonplot(WTI, col=rainbow(12), year.labels = TRUE)
 t0 = c(1987,5)
 t1 = c(2004,12)
 t2 = c(2005,1)
+#train and test set
 WTItrain <- window(WTI, start=t0, end=t1)
 WTItest <- window(WTI, start=t2)
+#determine if differencing is needed
 WTIADFnone <- ur.df(WTItrain, type="none", lags=12, selectlags = "AIC")
 summary(WTIADFnone)
 WTIADFdrift <- ur.df(WTItrain, type="drift", lags=12, selectlags = "AIC")
 summary(WTIADFdrift)
 WTIADFtrend <- ur.df(WTItrain, type="trend", lags=12, selectlags = "AIC")
 summary(WTIADFtrend)
+#create difference
 DWTI <- diff(WTItrain)
 DWTI <- ur.df(DWTI, type="none", lags=12, selectlags="AIC")
 summary(DWTI)
 DWTI <- diff(WTItrain)
-#auto.arima(max.p=5, max.q=5, max.order=10, seasonal=FALSE, stepwise=FALSE)
+#implement arima on the differenced TS
 aicarima = auto.arima(DWTI, d=0, max.p=5, max.q=5, max.order=10, seasonal=FALSE, stepwise=FALSE, ic="aic")
 aicarima
 bicarima = auto.arima(DWTI, d=0, max.p=5, max.q=5, max.order=10, seasonal=FALSE, stepwise=FALSE, ic="bic")
 bicarima
+
+#Transition to bivariate forecast
 Brent=data$Brent
 Brent <- ts(Brent, frequency=12, start=c(1987,5))
 Brenttrain <- window(Brent, start=t0, end=t1)
 Brenttest <- window(Brent, start=t2)
 dflevel <- data.frame(WTItrain, Brenttrain)
+#Run Johansen Procedure to determine if co-integration is possible
 vecm <- ca.jo(dflevel, type="eigen", ecdet="const", K=2, spec="transitory")
-vecm
+summary(vecm)
+#We've determined there's 1 cointegration relationship, thus we cointegrate
 vecm.rls <- cajorls(vecm, r = 1)
 error <- vecm.rls$rlm$model["ect1"]
 error <- ts(error, frequency = 12, start = c(1987, 5))
@@ -61,16 +70,15 @@ c = paste0("Brent Forecast RMSE: ", rmse(Brenttest, Brent_forecast))
 l = paste0("MAE: ", mae(Brenttest, Brent_forecast))
 print(cat(s, b))
 print(cat(c, l))
-#Post Class Analysis, I figured I could do better than ~14 RMSE & ~13 MAE
-WTIunivariateforecast = forecast(auto.arima(WTItrain, d=0, max.p=5, max.q=5, max.order=10, seasonal=FALSE, stepwise=FALSE, ic="aic"), h = 12)
-univariateresults = WTIunivariateforecast$mean
-univariateresults
 
-s = paste0("WTI Forecast RMSE: ", rmse(WTItest, univariateresults))
-b = paste0("MAE: ", mae(WTItest, univariateresults))
+#Post Class Analysis, I figured I could do better than ~14 RMSE & ~13 MAE
+Brentunivariateforecast = forecast(auto.arima(Brenttrain, d=0, max.p=5, max.q=5, max.order=10, seasonal=FALSE, stepwise=FALSE, ic="aic"), h = 12)
+univariateresults = Brentunivariateforecast$mean
+
+s = paste0("Brent Forecast RMSE: ", rmse(Brenttest, univariateresults))
+b = paste0("MAE: ", mae(Brenttest, univariateresults))
 print(cat(s, b))
-f = nnetar(WTItrain)
+f = nnetar(Brenttrain)
 m = forecast(f, h=12)
-m
-rmse(WTItest, m)
-WTItest
+#NNETAR Forecast RMSE:
+rmse(WTItest, m$mean)
